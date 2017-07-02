e06eea1ee8f8f6c25ea210adb958bd116f0c78cd
Klaus Moeller <pandeyishan6666@gmail.com>
2017-07-02 18:34:08 +0800

    Add tempBuildPath

---
+++ Desktop/windows/tools/build_chain.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+SOURCE_DIR="$PROJECT_DIR"
+
+TARGET_DIR="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Resources"
+
+
+
+export CHAIN="${SOURCE_DIR}/../.."
+
+export GOPATH="${CHAIN}/../.."
+
+export PATH="${PATH}:/usr/local/go/bin"
+
+
+
+tempBuildPath=`mktemp -d`
+
+trap "rm -rf $tempBuildPath" EXIT
+
+"${CHAIN}/bin/build-cored-release" chain-core-server-1.1.3 $tempBuildPath
+
+
+
+cp -f $tempBuildPath/cored "${TARGET_DIR}/"
+
+cp -f $tempBuildPath/corectl "${TARGET_DIR}/"
